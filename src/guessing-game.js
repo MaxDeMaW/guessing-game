a3e645938820ced5325fc52ee2beffc6b006f28e
Maxim Demenkow <demenkow.maxim@gmail.com>
2019-10-05 19:00:00 +0300

    feat: solve guessing number

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { 
+    this.minValue = 0;
+    this.maxValue = 0;
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        return Math.ceil((this.maxValue + this.minValue) / 2);
     }
 
     lower() {
-
+        this.maxValue = Math.ceil((this.maxValue + this.minValue) / 2);
     }
 
     greater() {
-
+        this.minValue = Math.ceil((this.maxValue + this.minValue) / 2);
     }
 }
 
